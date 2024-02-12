265afe6a71a5b7e1971e604d568b46f5aa2594c3
getrojob <getrojob@gmail.com>
2024-02-11 22:04:57 -0300

    add Query and commands

---
+++ src/CleanArch.Domain/Abstractions/IMemberDapperRepository.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using CleanArch.Domain.Entities;
+
+namespace CleanArch.Domain.Abstractions
+{
+    public interface IMemberDapperRepository
+    {
+        Task<IEnumerable<Member>> GetMembers();
+        Task<Member> GetMemberById(int id);
+    }
+}
